0b7d14f391d31ddd3fe9ff23e8c9571554199372
dongwooha <dongwoo.ha@gmail.com>
2022-03-19 17:53:57 +0900

    온보딩 작업

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:simple_mbti_store/onboarding.dart';
 
+// MBTI class를 하나 만들어두면 편하겠네요.
+// MBTI, 속성
+// (예시)
+// "ENTP", "상상력이 풍부하여 철두철미한 계획을 세우는 전략가형"
+
+// class를 기준으로 List를 하나 만들면 좋을 거 같아요.
+
 void main() {
   runApp(const MyApp());
 }
@@ -23,6 +30,9 @@ class AppListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("MBTI"),
+      ),
       body: SafeArea(
         child: Center(
           child: Column(
