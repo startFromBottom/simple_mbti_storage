98c056136426193dcdfc5e0ecdab09240139adfb
dongwooha <dongwoo.ha@gmail.com>
2022-03-19 15:14:58 +0900

    메인앱에서 다양한 화면으로 진입할 수 있도록 세팅했습니다.

--- lib/onboarding.dart
+++ lib/onboarding.dart
@@ -1,3 +1,27 @@
 // 온보딩 화면 구현합니다.
 // 로그인 -> 온보딩 -> 메인 으로 진행됩니다.
 // 온보딩은 1회 이상 완료하면 더 이상 보여지지 않습니다.
+
+import 'package:flutter/material.dart';
+
+class Onboarding extends StatelessWidget {
+  const Onboarding({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Column(
+            children: [
+              Text("onboarding"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
